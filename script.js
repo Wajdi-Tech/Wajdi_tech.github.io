e097f484fc0fa15dd141ea3cb36d3de9b5679a74
wajdi <wajdibargui8@gmail.com>
2024-10-21 01:10:29 +0200

    ajout mon version2 site web

---
+++ script.js
@@ -0,0 +1,16 @@
+/*
+const modal = document.getElementById('videoModal');
+const openButton = document.getElementById('videoButton');
+const closeButton = document.getElementById('closeButton');
+
+
+openButton.addEventListener('click', () => {
+    modal.classList.remove('hidden');
+});
+
+
+closeButton.addEventListener('click', () => {
+    modal.classList.add('hidden');
+});
+*/
+
